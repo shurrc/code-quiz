6cef5699cee8e54d3c8b4281603352b1c781fda5
Kathy Shurr <kathyshurr@kathys-mbp.myfiosgateway.com>
2022-10-05 07:31:46 -0400

    Formatted HTML file, made basic page structure. Psuedocoded JS file. added variables connected to each element in HTML file.

---
+++ assets/script.js
@@ -0,0 +1,56 @@
+//dependencies
+// button to start the game
+
+//container that displays question and choices
+//another container holding the timer
+//container for the highscores 
+    //form to record name
+    //submit button to confirm name
+    //reset button to reset global highscores
+var newGameButton = document.getElementById("new-game");
+var questionDisplay = document.getElementById("question-display");
+var question = document.getElementById("question");
+var choiceContainer = document.getElementById("Choices");
+var timeContainer = document.getElementById("time-container");
+var timer = document.getElementById("timer")
+var highscoreContainer = document.getElementById("highscore-conatiner")
+var scores = document.getElementById("scores");
+var resetButton = document.getElementById("reset-button")
+//Data
+
+//array with the question and answers
+//variable to conatin the wuestions
+//variable to hold the answers
+//variable to hold the actual timer
+//variable to hold the countdown of the remaining seconds
+//variable to hold recorded scores
+//variable to save scores in local storage
+
+//functions
+//
+
+
+//user experience 
+//The user will be able to click the start button
+    //once this happens the timer will start
+    //a question appears with multiple choice questions
+//user can click on the multiple choices to choose an answer
+    //when this happens the answer will be verified as correct or incorrect
+    //correct answers will award the user a point
+    //incorrect answer will subtract time from the clock
+    //a new question will appear after answer
+//when the user runs out of time or answers all the questions the game will end
+    //if the user answers all the questions the timer will stop
+//when the game ends the user will see a scoreboard of previous attemps
+    //the user will be able to record their name 
+    //their name along with their score will be recorded on scoreboard
+    //user can reset scoreboard with button
+//user can start a new game with start game button
+
+//intialization
+
+
+
+
+
+//intialization
